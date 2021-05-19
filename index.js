2d7dae6bf31f15a1d7149889eba8992e041fb273
MHenderson2001 <>
2021-05-19 17:35:50 +0000

    added LC04 report

--- index.js
+++ index.js
@@ -2,28 +2,108 @@
 
 
 // Declare and initialize the 12 variables here:
-let Date = Monday 2019-03-18
-let time = 10:05:34AM
-let astronautCount = 7
-let astronautStatue = ready
-let AstronautMassKg = 80.7
-let crewMassKg = astraunaut * averageAstronautMassKg
-let fuelMassKs = 760000
-let shuttleMassKg = 74842.31
-let totalMassKg = crewMassKg + fuelMassKG + shuttleMassKg
-let fuelTempCelsius = -225
-let fuelLevel = 100%
-let weatherStatus = clear
+
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg
+let fuelTempCelsius = -225;
+let fuelLevel = 1;
+let weatherStatus = "clear";
+
 
 
 
 
 
 // Write code to generate the LC04 report here:
+let theLine = "-------------------------------------"
+
+
+console.log(theLine);
+console.log("> LC04 - LAUNCH CHECKLIST");
+console.log(theLine);
+console.log("Date: " + date);
+console.log("Time: " + time);
+console.log();
+console.log(theLine);
+console.log("> ASTRONAUT INFO");
+console.log(theLine);
+console.log("* count: " + astronautCount);
+console.log("* status: " + astronautStatus);
+console.log();
+console.log(theLine);
+console.log("> FUEL DATA")
+console.log(theLine);
+console.log("* Fuel temp celsius: " + fuelTempCelsius + "C");
+console.log("* Fuel level: " + (fuelLevel * 100) + "%");
+console.log();
+console.log(theLine);
+console.log("> MASS DATA");
+console.log(theLine);
+console.log("* Crew mass: " + crewMassKg + "kg" );
+console.log("* Fuel mass: " + fuelMassKg + "kg");
+console.log("* Shuttle mass: " + shuttleMassKg + "kg");
+console.log("* Total mass: " + totalMassKg + "kg");
+console.log();
+console.log(theLine);
+console.log("> FLIGHT PLAN");
+console.log(theLine);
+console.log()
+console.log("weather: " + weatherStatus);
+console.log()
+console.log(theLine);
+console.log("> OVERALL STATUS");
+console.log(theLine);
+console.log("Clear for takeoff: YES")
 
 
 
 
+
+ /*
+-------------------------------------
+> LC04 - LAUNCH CHECKLIST
+-------------------------------------
+Date: Monday 2019-03-18
+Time: 10:05:34 AM
+
+-------------------------------------
+> ASTRONAUT INFO
+-------------------------------------
+* count: 7
+* status: ready
+
+-------------------------------------
+> FUEL DATA
+-------------------------------------
+* Fuel temp celsius: -225 C
+* Fuel level: 100%
+
+-------------------------------------
+> MASS DATA
+-------------------------------------
+* Crew mass: 564.9 kg
+* Fuel mass: 760000 kg
+* Shuttle mass: 74842.31 kg
+* Total mass: 835407.21 kg
+
+-------------------------------------
+> FLIGHT PLAN
+-------------------------------------
+* weather: clear
+
+-------------------------------------
+> OVERALL STATUS
+-------------------------------------
+* Clear for takeoff: YES
+
+*/
 // When done, have your TA check your code.
 
 
